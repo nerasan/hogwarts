c1d410996f3695933ca432c5c1c5e9bae43c2450
nerasan <salima.harun@gmail.com>
2020-09-08 18:25:33 -0500

    year 1 - added h1 using javascript

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 //     console.log('DOM is loaded!')
 // })
 
+const container = document.querySelector('#container')
+//console.log(container)
+
+const h1 = document.createElement('h1')
+console.log(h1)
+h1.innerText = 'welcome to hogwarts!'
+document.body.appendChild(h1)
