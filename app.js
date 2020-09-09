0840e02ceea06a72462e3128d55320e1021cf19f
nerasan <salima.harun@gmail.com>
2020-09-08 19:16:24 -0500

    year 3 - i added list elements. that code didn't seem very dry. i wonder if there is something i could do about it in the hungry for more section

--- app.js
+++ app.js
@@ -8,23 +8,31 @@ const container = document.querySelector('#container')
 const h1 = document.createElement('h1')
 //console.log(h1)
 h1.innerText = 'welcome to hogwarts!'
-document.body.appendChild(h1)
+container.appendChild(h1)
 
 const h2 = document.createElement('h2')
 h2.innerText = 'salima harun'
-document.body.appendChild(h2)
+container.appendChild(h2)
 
 const h3 = document.createElement('h3')
 h3.innerText = 'slytherin'
-document.body.appendChild(h3)
+container.appendChild(h3)
 
 const h4pet = document.createElement('h4')
 h4pet.innerText = 'kenma'
 h4pet.classList.add('cat')
 //console.log(h4pet)
-document.body.appendChild(h4pet)
+container.appendChild(h4pet)
 
 const h4wand = document.createElement('h4')
 h4wand.innerText = 'hawthorn wand with dragon heartstring core'
 //console.log(h4wand)
-document.body.appendChild(h4wand)
+container.appendChild(h4wand)
+
+// YEAR THREE
+
+const ul = document.createElement('ul')
+ul.setAttribute('storage', 'trunk')
+ul.innerHTML = '<li>butter beer</li><li class="secret">invisibility cloak</li><li class="secret">magic map</li><li class="secret">time turner</li><li class="cat">leash</li><li>bertie bott\'s every flavor [jelly] beans</li>'
+console.log(ul)
+container.appendChild(ul)
