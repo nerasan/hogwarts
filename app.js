1d2b3f51e3038098a73ab376c89363d8a553fee6
nerasan <salima.harun@gmail.com>
2020-09-08 20:28:13 -0500

    year 4 - i realize i can make things that are ever increasingly more complex with greater ease. adding order is always more effort than causing chaos.

--- app.js
+++ app.js
@@ -36,3 +36,79 @@ ul.setAttribute('storage', 'trunk')
 ul.innerHTML = '<li>butter beer</li><li class="secret">invisibility cloak</li><li class="secret">magic map</li><li class="secret">time turner</li><li class="cat">leash</li><li>bertie bott\'s every flavor [jelly] beans</li>'
 console.log(ul)
 container.appendChild(ul)
+
+// YEAR FOUR
+
+const table = document.createElement('table')
+container.appendChild(table)
+
+const h5 = document.createElement('h5')
+h5.innerText = 'spring 2017'
+container.insertBefore(h5, table)
+
+// THEAD -- DAYS AND CLASSES (headers)
+const thead = document.createElement('thead')
+table.appendChild(thead)
+
+    const thday = document.createElement('th')
+    thday.innerText = 'day'
+    thead.appendChild(thday)
+
+    const thclasses = document.createElement('th')
+    thclasses.innerText = 'classes'
+    thead.appendChild(thclasses)
+
+const tr1 = document.createElement('tr')
+table.appendChild(tr1)
+
+    const tdmonday = document.createElement('td')
+    tdmonday.innerText = 'monday'
+    tr1.appendChild(tdmonday)
+
+    const tdmondayclass = document.createElement('td')
+    tdmondayclass.innerText = 'herbology, charms, potions'
+    tr1.appendChild(tdmondayclass)
+
+const tr2 = document.createElement('tr')
+table.appendChild(tr2)
+
+    const tdtuesday = document.createElement('td')
+    tdtuesday.innerText = 'tuesday'
+    tr2.appendChild(tdtuesday)
+
+    const tdtuesdayclass = document.createElement('td')
+    tdtuesdayclass.innerText = 'defense against the dark arts, quidditch practice'
+    tr2.appendChild(tdtuesdayclass)
+
+const tr3 = document.createElement('tr')
+table.appendChild(tr3)
+
+    const tdwednesday = document.createElement('td')
+    tdwednesday.innerText = 'wednesday'
+    tr3.appendChild(tdwednesday)
+
+    const tdwednesdayclass = document.createElement('td')
+    tdwednesdayclass.innerText = 'history of magic, divination, transfiguration'
+    tr3.appendChild(tdwednesdayclass)
+
+const tr4 = document.createElement('tr')
+table.appendChild(tr4)
+
+    const tdthursday = document.createElement('td')
+    tdthursday.innerText = 'thursday'
+    tr4.appendChild(tdthursday)
+
+    const tdthursdayclass = document.createElement('td')
+    tdthursdayclass.innerText = 'defense against the dark arts, quidditch practice'
+    tr4.appendChild(tdthursdayclass)
+
+const tr5 = document.createElement('tr')
+table.appendChild(tr5)
+
+    const tdfriday = document.createElement('td')
+    tdfriday.innerText = 'friday'
+    tr5.appendChild(tdfriday)
+
+    const tdfridayclass = document.createElement('td')
+    tdfridayclass.innerText = 'astronomy, muggle studies, care of magical creatures'
+    tr5.appendChild(tdfridayclass)
